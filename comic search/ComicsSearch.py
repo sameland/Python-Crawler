6d583ff0636f675918806ae9adcc688fcdf9d101
zhangjj <zhangjj@ucweb.com>
2015-11-16 11:37:12 +0800

    create comic search

---
+++ comic search/ComicsSearch.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+#读取目录下的list.txt内的漫画列表,获取极速漫画的每日更新
+__author__ = 'sameland'
+import time,urllib2,re,cookielib
+
+class ComicsSearch:
+    host = "http://www.1kkk.com"
+    newest_page = "http://www.1kkk.com/manhua-new"
+    name_re = r'<br />([\x21-\xff]+)</a><span class="lj_h1">'
+    time_re = r'</span><br />([\x80-\xff,\s,a-z,0-9,A-Z,-]+)<span class="flowtag">'
+    url_re = r'<span class="lj_h1"><a href="([a-z,0-9,A-Z,//,-]+)">'
+    comics_list = None
+    url_result = None
+    time_result = None
+    name_result = None
+    content = None
+    opener = None
+    curr_time = None
+    page = "-p"
+    count = 2
+    isContinue = True
+
+    def __init__(self):
+        file = open('list.txt','r')
+        file_content = file.read().decode('gbk').encode('utf-8')
+        self.comics_list = file_content.split('，')
+        file.close()
+        self.curr_time = time.strftime('%Y-%m-%d',time.localtime(time.time()))
+        cookie = cookielib.CookieJar()
+        cookie_handler = urllib2.HTTPCookieProcessor(cookie)
+        self.opener = urllib2.build_opener(cookie_handler)
+        urllib2.install_opener(self.opener)
+
+    def request_content(self,url):
+        content = self.opener.open(url)
+        content = content.read()
+        name_pattern = re.compile(self.name_re)
+        self.name_result = name_pattern.findall(content)
+        time_pattern = re.compile(self.time_re)
+        self.time_result = time_pattern.findall(content)
+        url_pattern = re.compile(self.url_re)
+        self.url_result = url_pattern.findall(content)
+
+    def filter_comics(self):
+        while self.isContinue:
+             for idx, val in enumerate(self.time_result):
+                    tmp_name = self.name_result[idx]
+                    if val.__contains__(self.curr_time):
+                        if tmp_name in self.comics_list:
+                            print self.host+self.url_result[idx]
+                        if idx == self.url_result.__len__()-1:
+                            url = self.newest_page + self.page + str(self.count)
+                            self.request_content(url)
+                            self.count += 1
+                    else:
+                        self.isContinue = False
+                        break
+
+    def main(self):
+        self.request_content(self.newest_page)
+        self.filter_comics()
+
+
+if __name__ == '__main__':
+    comcis = ComicsSearch()
+    comcis.main()
